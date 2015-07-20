a47884cfc67ad92b31a0039c9892b267fb79ca19
Thomas Lee <lisihan969@gmail.com>
2015-07-20 15:58:00 +0800

    Implement logic.v

--- src/logic.v
+++ src/logic.v
@@ -1,10 +1,23 @@
 module logic(
-    output [31:0] dout,
+    output reg [31:0] dout,
     input [31:0] A,
     input [31:0] B,
     input [3:0] ctrl
 );
 
-
+always @(*) begin
+    case (ctrl)
+        4'b1000:  // AND: S = A & B.
+            dout = A & B;
+        4'b1110:  // OR: S = A | B.
+            dout = A | B;
+        4'b0110:  // XOR: S = A ^ B.
+            dout = A ^ B;
+        4'b0001:  // NOR: S = ~(A | B).
+            dout = ~(A | B);
+        4'b1010:  // "A": S = A.
+            dout = A;
+    endcase
+end
 
 endmodule
