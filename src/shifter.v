1c3b91aa5483dda08f11b4dcf22704163a57c35b
Thomas Lee <lisihan969@gmail.com>
2015-07-20 16:11:52 +0800

    Implement fix_shifter

--- src/shifter.v
+++ src/shifter.v
@@ -1,10 +1,32 @@
 module shifter(
-    output [31:0] dout,
+    output reg [31:0] dout,
     input [31:0] A,
     input [31:0] B,
     input [1:0] ctrl
 );
 
 
+
+endmodule
+
+
+module fix_shifter(
+    output [31:0] dout,
+    input [31:0] B,
+    input [1:0] ctrl
+);
+
+parameter SHIFT_AMOUNT = 1;
+
+always @(ctrl)
+    case (ctrl)
+        2'b00:  // SLL: S = B << A[4:0].
+            dout = B << SHIFT_AMOUNT;
+        2'b01:  // SRL: S = B >> A[4:0].
+            dout = B >> SHIFT_AMOUNT;
+        default:  // SRA: S = B >> A[4:0] 算数移位.
+            dout = B >>> SHIFT_AMOUNT;
+    endcase
+end
 
 endmodule
