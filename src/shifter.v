d7674854d9e7bcf679b0df46bdd99bdbf6d3a529
Thomas Lee <lisihan969@gmail.com>
2015-07-20 23:04:20 +0800

    Fix the implement of fix_shifter
    Make dout a reg & add enable port.

--- src/shifter.v
+++ src/shifter.v
@@ -11,22 +11,26 @@ endmodule
 
 
 module fix_shifter(
-    output [31:0] dout,
+    output reg [31:0] dout,
     input [31:0] B,
-    input [1:0] ctrl
+    input [1:0] ctrl,
+    input enable
 );
 
 parameter SHIFT_AMOUNT = 1;
 
-always @(ctrl)
-    case (ctrl)
-        2'b00:  // SLL: S = B << A[4:0].
-            dout = B << SHIFT_AMOUNT;
-        2'b01:  // SRL: S = B >> A[4:0].
-            dout = B >> SHIFT_AMOUNT;
-        default:  // SRA: S = B >> A[4:0] 算数移位.
-            dout = B >>> SHIFT_AMOUNT;
-    endcase
+always @(*)
+    if (enable)
+        case (ctrl)
+            2'b00:  // SLL: S = B << A[4:0].
+                dout = B << SHIFT_AMOUNT;
+            2'b01:  // SRL: S = B >> A[4:0].
+                dout = B >> SHIFT_AMOUNT;
+            default:  // SRA: S = B >> A[4:0] 算数移位.
+                dout = B >>> SHIFT_AMOUNT;
+        endcase
+    else
+        dout = B;
 end
 
 endmodule
